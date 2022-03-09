2e85f87f2ca079cf6240df3d6a85766465a45c80
SahilKhan30 <93254650+SahilKhan30@users.noreply.github.com>
2022-03-09 21:32:03 +0530

    Add files via upload

---
+++ car_manager.py
@@ -0,0 +1,32 @@
+import turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+
+
+class CarManager:
+
+    def __init__(self):
+        self.list_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        chance = random.randint(1, 6)
+        if chance == 1:
+            new_car = turtle.Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.goto(300, random.randint(-250, 250))
+            new_car.setheading(-180)
+            self.list_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.list_cars:
+            car.forward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(fun=player.move_up, key="Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # detects collision with car
+    for car in car_manager.list_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # detects successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.update_scoreboard()
+
+
+screen.exitonclick()
